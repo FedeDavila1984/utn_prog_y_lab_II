59aa6a5efc8ea70a501ad57a4fbd750f18e5c5ef
Federico Dávila <federicordavila@gmail.com>
2017-04-04 16:25:12 -0300

    Clase 6 v2

---
+++ MostradosEnClase/Ej.Colecciones/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej.Colecciones
+{
+    class Cliente
+    {
+        string _nombre;
+
+        public Cliente(string nombre)
+        {
+            this._nombre = nombre;
+        }
+
+        public override string ToString()
+        {
+            return this._nombre;
+        }
+    }
+}

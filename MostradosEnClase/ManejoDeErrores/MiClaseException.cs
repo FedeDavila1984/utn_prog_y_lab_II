ac3a6b7a7f445ff79df73357bb3a7bee778d2cef
profesor <profesor>
2017-05-09 21:46:40 -0300

    Excepciones

---
+++ MostradosEnClase/ManejoDeErrores/MiClaseException.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManejoDeErrores
+{
+    class MiClaseException : Exception
+    {
+        
+        /// <summary>
+        /// CONSTRUCTOR POR DEFECTO
+        /// </summary>
+        public MiClaseException():base()
+        {
+        }
+    
+        /// <summary>
+        /// PASO EL MENSAJE DE ERROR PERSONALIZADO
+        /// </summary>
+        /// <param name="mensaje">MENSAJE DE ERROR PERSONALIZADO</param>
+        public MiClaseException(string mensaje) : base(mensaje) 
+        { 
+        }
+
+        /// <summary>
+        /// PASO EL MENSAJE DE ERROR PERSONALIZADO
+        /// Y LA CLASE QUE PROVOCO LA EXCEPCION
+        /// </summary>
+        /// <param name="mensaje">MENSAJE DE ERROR PERSONALIZADO</param>
+        /// <param name="origenError">CLASE QUE PROVOCO LA EXCEPCION</param>
+        public MiClaseException(string mensaje,string origenError)
+            : base(mensaje)
+        {
+            base.Source = origenError;
+        }
+
+        /// <summary>
+        /// PASO EL MENSAJE DE ERROR PERSONALIZADO
+        /// Y LA EXCEPCION 
+        /// </summary>
+        /// <param name="mensaje">MENSAJE DE ERROR PERSONALIZADO</param>
+        /// <param name="innerException">EXCEPCION</param>
+        public MiClaseException(string mensaje, Exception innerException)
+            : base(mensaje, innerException) 
+        { 
+        }
+
+        /// <summary>
+        /// PASO EL MENSAJE DE ERROR PERSONALIZADO,
+        /// LA CLASE QUE PROVOCO LA EXCEPCION
+        /// Y LA EXCEPCION 
+        /// </summary>
+        /// <param name="mensaje">MENSAJE DE ERROR PERSONALIZADO</param>
+        /// <param name="origenError">CLASE QUE PROVOCO LA EXCEPCION</param>
+        /// <param name="innerException">EXCEPCION</param>
+        public MiClaseException(string mensaje, string origenError, Exception innerException)
+            : base(mensaje, innerException)
+        {
+            base.Source = origenError;        
+        }
+
+        //SOBREESCRIBO EL METODO TOSTRING
+        public override string ToString()
+        {
+           return "Mensaje: " + base.Message + ", Origen del error: " + base.Source;
+        }
+    }
+}
---
+++ MostradosEnClase/ManejoDeErrores/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManejoDeErrores
+{
+    /// <summary>
+    /// Original por Maximiliano Neiner
+    /// Modificaciones 2017 por Federico Dávila
+    /// </summary>
+    class Program
+    {
+        public static void ArrojarExcepcion()
+        {
+            throw new MiClaseException("Error personalizado!!!",
+                                       "Class Program.ArrojarExcepcion");
+        }
+
+        public static double Dividir(int numero1, int numero2)
+        {
+            double resultado=0;
+            try
+            {
+                resultado = numero1 / numero2;
+            }
+            catch (DivideByZeroException ex) // 
+            {
+                throw new MiClaseException("Error al intentar dividir!!!",
+                                           "Class Program.Dividir",
+                                           ex);
+                //Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Bloque Finally");
+            }
+
+            // Si el resultado de la división es menor a 1, lanzo la excepción
+            if (resultado < 1)
+                throw new Exception("Resultado menor a 1");
+
+            return resultado;
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                double res;
+
+                ArrojarExcepcion(); // Comentar para seguir probando
+
+                res = Dividir(1, 0); // Comentar para seguir probando
+
+                res = Dividir(1, 5); // Comentar para seguir probando
+
+                res = Dividir(1, 1);
+                Console.WriteLine("Resultado: {0}", res);
+            }
+            catch (MiClaseException ex) //CAPTURO EXCEPCION PERSONALIZADA
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("InnerException: {0}", ex.InnerException);
+            }
+            catch (Exception ex) //CAPTURO CUALQUIER EXCEPCION
+            {
+                Console.WriteLine(ex.Message);
+            }
+                            
+            Console.ReadLine();
+
+        }
+
+    }
+
+
+}
+

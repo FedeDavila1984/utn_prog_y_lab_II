d32787a5ac30e6c872b130bfb5b16a96d083f340
profesor <profesor>
2017-08-24 22:08:45 -0300

    Clase 4

---
+++ MostradosEnClase/Clase-4/Clase-4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador s1 = new Sumador();
+            Console.WriteLine(s1.Sumar("a", "n"));
+            Console.WriteLine(s1.Sumar(1, 10001));
+            Sumador s2 = new Sumador(19);
+
+            Console.WriteLine("Suma de Sumadores");
+            Console.WriteLine(s1 + s2);
+            Console.WriteLine("Explicit de Sumador s1");
+            Console.WriteLine((int)s1);
+            Console.WriteLine("Pipe de Sumadores s1 | s2");
+            Console.WriteLine(s1 | s2);
+            Console.WriteLine("Pipe de Sumadores s2 | s2");
+            Console.WriteLine(s2 | s2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MostradosEnClase/Clase-4/Clase-4/Sumador.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_4
+{
+    class Sumador
+    {
+        int cantidadSumas;
+
+        public Sumador()
+        {
+            this.cantidadSumas = 0;
+        }
+
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+
+        public long Sumar(long a, long b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+        public string Sumar(string a, string b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+
+        public static explicit operator int(Sumador s)
+        {
+            return s.cantidadSumas;
+        }
+
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            return s1.cantidadSumas + s2.cantidadSumas;
+        }
+
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            return (s1.cantidadSumas == s2.cantidadSumas);
+        }
+
+    }
+}

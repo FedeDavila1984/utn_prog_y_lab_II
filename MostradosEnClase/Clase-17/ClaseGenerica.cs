ddf9015790bc43fea05285b6f461bea0cf54cef0
profesor <profesor>
2017-10-11 22:13:19 -0300

    20171011 v2

--- MostradosEnClase/Clase-17/ClaseGenerica.cs
+++ MostradosEnClase/Clase-17/ClaseGenerica.cs
@@ -7,8 +7,9 @@ using System.Threading.Tasks;
 namespace Clase_17
 {
     public class ClaseGenerica<T, U, V>
-        where T : MetodosGenericos
-        where T : new()
+        where T : MetodosGenericos, new()
+        //where U : MetodosGenericos
+        //where T : new()
     {
         public ClaseGenerica<T, U, V> MetodoConElTipo(ClaseGenerica<T, U, V> cg)
         {

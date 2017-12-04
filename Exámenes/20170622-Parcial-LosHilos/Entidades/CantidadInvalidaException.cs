80aefebaf19b40c6121f1f0db95308a05ed1e0c8
Federico Dávila <federicordavila@gmail.com>
2017-12-04 11:02:21 -0300

    Examenes

---
+++ Exámenes/20170622-Parcial-LosHilos/Entidades/CantidadInvalidaException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class CantidadInvalidaException : Exception
+    {
+        public CantidadInvalidaException() : base("La cantidad debe ser de al menos uno (1).") { }
+    }
+}
---
+++ Exámenes/20170622-Parcial-LosHilos/Parcial-2-20170622/frmPpal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Entidades;
+using System.Threading;
+
+namespace Parcial_2_20170622
+{
+    public partial class frmPpal : Form
+    {
+        LosHilos hilos;
+        public frmPpal()
+        {
+            InitializeComponent();
+
+            hilos = new LosHilos();
+            hilos.AvisoFin += this.MostrarMensajeFin;
+        }
+
+        private void btnLanzar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                hilos += 1;
+            }
+            catch (CantidadInvalidaException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void MostrarMensajeFin(string mensaje)
+        {
+            MessageBox.Show(mensaje);
+        }
+
+        private void btnBitacora_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MessageBox.Show(this.hilos.Bitacora);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}

f77349c0cfbe5b5d010824720263e893d2076907
profesor <profesor>
2018-03-21 21:17:16 -0300

    20180321

---
+++ Guia2017/Ej.001/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej._001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalNumeroALeer = 5;
+
+            int aux;
+            int max = 0;
+            int min = 0;
+            float promedio = 0;
+            string lectura;
+
+            for (int i = 0; i < totalNumeroALeer; i++)
+            {
+                Console.Write("Ingrese un número: ");
+                lectura = Console.ReadLine();
+                if (int.TryParse(lectura, out aux))
+                {
+                    if (i == 0)
+                    {
+                        max = aux;
+                        min = aux;
+                        promedio = aux;
+                    }
+                    else
+                    {
+                        if (aux > max)
+                            max = aux;
+                        if (aux < min)
+                            min = aux;
+                        promedio = (promedio + aux);
+                    }
+                }
+                else
+                    i--;
+            }
+            promedio = promedio / totalNumeroALeer;
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Máximo : {0}", max);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Mínimo : {0}", min);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Promedio: {0:#.##}", promedio);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Guia2017/Ej.003/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej._003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aux;
+            Console.Write("Ingrese un número: ");
+            if (int.TryParse(Console.ReadLine(), out aux))
+            {
+                int i = 0;
+                // Analizo todos los números entre 2 y el número ingresado
+                for (int j = 2; j <= aux; j++)
+                {
+                    // Verifico si J es primo
+                    for (i = 2; i < j; i++)
+                    {
+                        // Si tiene un divisor con resto cero, salgo ya que NO ES PRIMO
+                        if ((j % i) == 0)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (i == j)
+                        Console.WriteLine("{0} es primo", j);
+                    //else
+                    //    Console.WriteLine("{0} NO es primo", j);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

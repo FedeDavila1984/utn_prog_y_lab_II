099be592734da39a60bd02dfba9cebcb7483ea92
profesor <profesor>
2018-04-03 21:45:29 -0300

    20180403

---
+++ MostradosEnClase/Clase-5-Igualdad-Objetos/MiClase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_5_Igualdad_Objetos
+{
+    public class MiClase
+    {
+        int miAlgo;
+
+        public MiClase(int algo)
+        {
+            this.miAlgo = algo;
+        }
+
+        public static bool operator ==(MiClase clase1, MiClase clase2)
+        {
+            if (clase1.miAlgo == clase2.miAlgo)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(MiClase clase1, MiClase clase2)
+        {
+            return !(clase1 == clase2);
+        }
+    }
+}
---
+++ MostradosEnClase/Clase-5-Igualdad-Objetos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_5_Igualdad_Objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MiClase m1 = new MiClase(1);
+            MiClase m2 = new MiClase(1);
+            MiClase m3 = m1;
+
+            if (!object.ReferenceEquals(m2, null))
+                Console.WriteLine("gola");
+
+            if (m1 == m2)
+                Console.WriteLine("m1 y m2 son iguales");
+            if (m1 == m3)
+                Console.WriteLine("m1 y m3 son iguales");
+            if (m2 == m3)
+                Console.WriteLine("m2 y m3 son iguales");
+
+            Console.ReadKey();
+        }
+    }
+}

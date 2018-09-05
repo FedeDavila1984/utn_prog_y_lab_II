6daaab720509e2233a476899942f6c315c97f454
profesor <profesor>
2018-09-04 21:08:34 -0300

    20180904

---
+++ MostradosEnClase/Ej.Null/Producto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej.Null
+{
+    public class Producto
+    {
+        private string marca;
+        private string codigoDeBarra;
+        private float precio;
+
+        /// <summary>
+        /// Inicializo un nuevo Producto, asignando todos sus atributos
+        /// </summary>
+        /// <param name="marca">Marca comercial del Producto</param>
+        /// <param name="codigo">Código de Barras del Producto</param>
+        /// <param name="precio">Valor comercial del Producto</param>
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigo;
+            this.precio = precio;
+        }
+
+        /// <summary>
+        /// Comparo dos objetos Producto a través de su Marca y Código de Barras
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <returns>True si Marca y Código de Barras son iguales</returns>
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            if (p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Comparo dos objetos Producto a través de su Marca y Código de Barras
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <returns>True si Marca y Código de Barras son distintos</returns>
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+    }
+}
---
+++ MostradosEnClase/Ej.Null/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej.Null
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producto prod = new Producto("Pepsi", "PESDS97413", (float)10.5);
+            Producto prodNull = null;
+
+            // Este IF da error!
+            if (prod == null)
+                Console.WriteLine("Esta comparación está mal!");
+
+            // Formas correctas
+            if (prodNull is null)
+                Console.WriteLine("Comparación IS");
+
+            if (Object.ReferenceEquals(prodNull, null))
+                Console.WriteLine("Comparación Object.ReferenceEquals");
+
+            if (Object.Equals(prodNull, null))
+                Console.WriteLine("Comparación Equals");
+        }
+    }
+}

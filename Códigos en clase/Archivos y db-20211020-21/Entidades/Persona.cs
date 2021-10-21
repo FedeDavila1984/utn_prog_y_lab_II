31e4ed5e56f91f7af25112d2f568b53a3b01c941
Fede Davila <federicordavila@gmail.com>
2021-10-21 20:16:19 -0300

    archivos y db en clase

---
+++ Códigos en clase/Archivos y db-20211020-21/Entidades/Persona.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace Entidades
+{
+    public class Persona
+    {
+        private static SqlConnection conn;
+        private static SqlCommand command;
+
+        private string nombre;
+        private int edad;
+        private bool vive;
+
+        static Persona()
+        {
+            string connString = @"Data Source=.\SQLEXPRESS; Initial Catalog=Empresa; Integrated Security=True";
+
+            Persona.conn = new SqlConnection(connString);
+
+            Persona.command = new SqlCommand();
+            Persona.command.CommandType = System.Data.CommandType.Text;
+            Persona.command.Connection = Persona.conn;
+        }
+
+        public string Nombre
+        {
+            get => this.nombre;
+            set => this.nombre = value;
+        }
+        public int Edad
+        {
+            get => this.edad;
+            set => this.edad = value;
+        }
+        public bool Vive
+        {
+            get => this.vive;
+            set => this.vive = value;
+        }
+
+        public static List<Persona> GetLista()
+        {
+            List<Persona> personas = new List<Persona>();
+
+            Persona.command.CommandText = "Select * From personas"; // nombre,edad,estadoDeVida
+
+            try
+            {
+                Persona.conn.Open();
+                // Persona.command.Connection.Open();
+
+                SqlDataReader reader = Persona.command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Persona p = new Persona();
+
+                    // p.Nombre = reader.GetString(0);
+
+                    //p.Nombre = reader[0].ToString();
+                    //p.Edad = int.Parse(reader[1].ToString());
+                    //p.Vive = bool.Parse(reader[2].ToString());
+
+                    p.Nombre = reader["nombre"].ToString();
+                    p.Edad = int.Parse(reader["edad"].ToString());
+                    p.Vive = bool.Parse(reader["estadoDeVida"].ToString());
+
+                    personas.Add(p);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                Persona.conn.Close();
+            }
+
+            return personas;
+        }
+
+        public static void Delete(decimal id)
+        {
+            Persona.command.CommandText = "Delete From personas Where id = @ID";
+            Persona.command.Parameters.Add(new SqlParameter("ID", id));
+
+            try
+            {
+                Persona.conn.Open();
+
+                Persona.command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                Persona.conn.Close();
+            }
+        }
+    }
+}
---
+++ Códigos en clase/Archivos y db-20211020-21/Forms/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+using System.Text.Json;
+
+namespace Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Persona> personas = new List<Persona>();
+            Persona p1 = new Persona();
+            p1.Nombre = "Pepe";
+            p1.Edad = 93;
+            p1.Vive = false;
+            Persona p2 = new Persona();
+            p2.Nombre = "Lola";
+            p2.Edad = 23;
+            p2.Vive = true;
+
+            personas.Add(p1);
+            personas.Add(p2);
+
+            string json = JsonSerializer.Serialize(personas);
+
+            richTextBox1.Text = json;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            List<Persona> personas = JsonSerializer.Deserialize<List<Persona>>(richTextBox1.Text);
+
+            richTextBox1.Text = "";
+            foreach (Persona p in personas)
+            {
+                richTextBox1.Text += p.Nombre + '\n';
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<Persona> personas = Persona.GetLista();
+
+                foreach (Persona p in personas)
+                {
+                    richTextBox1.Text += p.Nombre + '\n';
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Persona.Delete(nudId.Value);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
